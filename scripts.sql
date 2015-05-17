a75617243dcbd2286c4d4a2eeef6316318b86400
diedev <dam137@gmail.com>
2015-05-17 03:54:04 -0600

    Script fix
    Ya crea la tabla usuarios
    Ya crea la tabla noticias

--- scripts.sql
+++ scripts.sql
@@ -1,22 +1,24 @@
-CREATE OR REPLACE DATABASE `bd_practica` /*!40100 DEFAULT CHARACTER SET utf8 */;
+drop DATABASE bd_practica;
+CREATE DATABASE bd_practica /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE bd_practica;
-
-CREATE OR REPLACE TABLE `usuarios`(
-  `id_usuario` varchar(20) NOT NULL,
-  `clave` varchar(20) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `ultimo_acceso` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `prioridad` int(11) NOT NULL,
-  PRIMARY KEY (`id_usuario`)
+/*drop TABLE usuarios;*/
+CREATE TABLE usuarios(
+  id_usuario varchar(20) NOT NULL,
+  clave varchar(20) NOT NULL,
+  nombre varchar(45) NOT NULL,
+  ultimo_acceso TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  prioridad int(11) NOT NULL,
+  PRIMARY KEY (id_usuario)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE OR REPLACE TABLE `noticias`(
-  `id_usuario` varchar(20) NOT NULL,
-  `noticia` varchar(250) NOT NULL,
-  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`fecha`)
+/*drop TABLE noticias;*/
+CREATE TABLE noticias(
+  id_usuario varchar(20) NOT NULL,
+  noticia varchar(250) NOT NULL,
+  fecha TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (fecha)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `usuarios`(`id_usuario`, `clave`, `nombre`, `ultimo_acceso`, `prioridad`) VALUES ('admin','admin','Administrador',CURRENT_TIMESTAMP,0);
-INSERT INTO `noticias`(`id_usuario`, `noticia`, `fecha`) VALUES ('admin','primera noticia',CURRENT_TIMESTAMP);
+INSERT INTO usuarios(id_usuario, clave, nombre, ultimo_acceso, prioridad) VALUES ('admin','admin','Administrador',CURRENT_TIMESTAMP,0);
+INSERT INTO noticias(id_usuario, noticia, fecha) VALUES ('admin','primera noticia',CURRENT_TIMESTAMP);
